ae9ae21585f5c8b58cd39706850fe9f5f56e106f
5940rr <5940rr@gmail.com>
2024-08-20 16:46:23 +0800

    [refactor]

--- computer_vision/mediapipe_cv_tools.py
+++ computer_vision/mediapipe_cv_tools.py
@@ -21,7 +21,7 @@ def init_gesture_recognizer(model_path): # 初始化你的手勢辨識模型
     )
     return GestureRecognizer.create_from_options(options)
 
-def recognize_gesture(cv2_frame):
+def recognize_gesture(model, cv2_frame):
     # https://ai.google.dev/edge/api/mediapipe/python/mp/Image
     # cv2 frame -> mp.Image
     mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=cv2_frame)
@@ -48,7 +48,7 @@ def recognize_gesture_realtime(model, camera_id):
             put_cv2_text(show_frame, f"Collecting: {is_collection_start}", (30, 50)) # 顯示出是否蒐集中?
             if is_collection_start: # 要蒐集
                 # 辨識手勢
-                top_gesture, score = recognize_gesture(frame)
+                top_gesture, score = recognize_gesture(model, frame)
                 put_cv2_text(show_frame, f"Category: {top_gesture} - {round(score*100, 2)}%", (30, 100)) # 顯示出蒐集類別?
                 key = cv2.waitKey(100)
             else: # 不蒐集
@@ -66,8 +66,64 @@ def recognize_gesture_realtime(model, camera_id):
             is_collection_start = False
     cv2.destroyWindow(window_name)
 
+
+def init_face_detector(model_path):
+    FaceDetector = mp.tasks.vision.FaceDetector
+    FaceDetectorOptions = mp.tasks.vision.FaceDetectorOptions
+    BaseOptions = mp.tasks.BaseOptions
+    VisionRunningMode = mp.tasks.vision.RunningMode
+    with open(model_path, 'rb') as model:
+        model_file = model.read()
+    # 組合你的各種設定
+    options = GestureRecognizerOptions(
+        base_options=BaseOptions(model_asset_buffer=model_file), # 直接讀模型的binary內容，丟給物件。
+        running_mode=VisionRunningMode.IMAGE
+    )
+    return GestureRecognizer.create_from_options(options)
+
+def detect_face(model, cv2_frame):
+    # https://ai.google.dev/edge/api/mediapipe/python/mp/Image
+    # cv2 frame -> mp.Image
+    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=cv2_frame)
+    face_detection_result = model.recognize(mp_image)
+    print
+    return "None", 1.0
+
+def detect_face_realtime(model, camera_id):
+    window_name = "Face Detection"
+    camera = cv2.VideoCapture(camera_id)
+    is_collection_start = False # 預設不會一開始就蒐集
+    while True:
+        is_success, frame = camera.read()  # 從camera取得資料
+        if is_success:
+            show_frame = frame.copy() # Copy frame for display
+            put_cv2_text(show_frame, f"Collecting: {is_collection_start}", (30, 50)) # 顯示出是否蒐集中?
+            if is_collection_start: # 要蒐集
+                face, score = recognize_gesture(model, frame)
+                put_cv2_text(show_frame, f"Category: {top_gesture} - {round(score*100, 2)}%", (30, 100)) # 顯示出蒐集類別?
+                key = cv2.waitKey(100)
+            else: # 不蒐集
+                key = cv2.waitKey(1)
+            cv2.imshow(window_name, show_frame)
+        else:
+            print("Wait for camera ready......")
+            key = cv2.waitKey(1000)
+
+        if key == ord('q') or key == ord("Q"): # 中止
+            break
+        elif key == ord('a') or key == ord('A'): # 開始
+            is_collection_start = True
+        elif key == ord('z') or key == ord('Z'): # 暫停
+            is_collection_start = False
+    cv2.destroyWindow(window_name)
+    
+
 if __name__ == '__main__':
     model_path = 'gesture_recognizer.task'
     camera_id = 0
     model = init_gesture_recognizer(model_path)
     recognize_gesture_realtime(model, camera_id)
+
+    model_path = "cv_models/xxx.task"
+    face_detection_model = init_face_detector(model_path)
+    
